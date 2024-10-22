611f04d77f5567af53d7c109d6ab0a687ba5f9ab
4V1d03 <aguilarcastillo512@gmail.com>
2024-10-22 09:52:38 -0600

    feat: Parte 2
    BD ecommerce creada, conexion y usuario de la BD a utilizar, Tabla de productos, sales, novedades y sus insert respectivos, Dao de Products modificado para extraer la info de la BD, Controlador Home modifficado para aplicar css, y css de productos creado para la vista

--- docs/scripts/00_database.sql
+++ docs/scripts/00_database.sql
@@ -1,1 +1,2 @@
+-- Active: 1729610458113@@127.0.0.1@3307@ecommerce
 CREATE SCHEMA `ecommerce` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ docs/scripts/03_createUser.sql
@@ -0,0 +1,2 @@
+  CREATE USER 'ecommerce'@'%' IDENTIFIED BY 'ecommerce';
+  GRANT SELECT, INSERT, UPDATE, DELETE ON ecommerce.* TO 'ecommerce'@'%';
